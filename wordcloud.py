6704fe0f8266c71e0211f9567d61e817c6fc34ee
citlaline <168496446+citlaline@users.noreply.github.com>
2024-05-14 15:12:15 -0300

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -2,11 +2,14 @@ import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-text = input("Digite a letra de uma música sem pontuações: ")
-
-wordcloud = WordCloud().generate(text.lower())
+texto = input("Digite a letra de uma música sem pontuações: ")
+wordcloud = WordCloud(width=800, height=400).generate(texto.lower())
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 st.pyplot()
+
+wordcloud.to_file("wordcloud.png")
+
+st.image("wordcloud.png", use_column_width=True)
