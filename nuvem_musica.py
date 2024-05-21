c8c05085964ebaab53f7ceb794286f9c6068848d
citlaline <168496446+citlaline@users.noreply.github.com>
2024-05-20 22:38:10 -0300

    Update nuvem_musica.py

--- nuvem_musica.py
+++ nuvem_musica.py
@@ -3,13 +3,9 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 texto = st.text_input("Digite a letra de uma música sem pontuações: ")
-wc = WordCloud(width=800, height=400).generate(texto.lower())
-
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-st.pyplot()
-
-wc.to_file("wc.png")
-
-st.image("wc.png", use_column_width=True)
+if len(texto) > 0:
+    wc = WordCloud(width=800, height=400).generate(texto.lower())
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    st.pyplot(plt)
